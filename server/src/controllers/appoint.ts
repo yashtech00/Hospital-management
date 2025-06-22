89c6bc8bcf7b176508a06f2a9db323f7833b1f54
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-06-22 21:40:00 +0530

    update

---
+++ server/src/controllers/appoint.ts
@@ -0,0 +1,10 @@
+
+export const BookAppointment = async (req: any, res: any) => {
+    try {
+        const appointPayload = req.body;
+        const appointParsed = appoinProp.safeParse(appointPayload);
+        const newappoint = await ()
+    } catch (e) {
+        
+    }
+}
