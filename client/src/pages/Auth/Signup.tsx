2e0dc06aad443cb362ac4b729ebd269d85b9eba2
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-07-04 16:54:46 +0530

    update

---
+++ client/src/pages/Auth/Signup.tsx
@@ -0,0 +1,18 @@
+
+
+
+
+export default function Signup() {
+    return (
+        <div>
+            <div>
+                <form>
+                    <span>Signup</span>
+                    <div>
+                        
+                    </div>
+                </form>
+           </div>
+        </div>
+    )
+}
